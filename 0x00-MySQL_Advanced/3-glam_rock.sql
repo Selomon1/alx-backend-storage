ad8a0077192af29841d6e036984375c53a91dad7
Selomon1 <soya.vandy1950@gmail.com>
2024-03-21 20:14:40 +0300

    MySQL Advanced

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,5 +1,7 @@
+-- Query to list bands with Glam rock as their main style
+
 SELECT band_name,
-	GREATEST(IFNULL(split, 2022) - formed, 0) AS lifespan
+        COALESCE(split, 2022) - formed AS lifespan
 FROM metal_bands
-Where style LIKE '%Glam rock%'
+WHERE BINARY style LIKE '%Glam rock%'
 ORDER BY lifespan DESC;
