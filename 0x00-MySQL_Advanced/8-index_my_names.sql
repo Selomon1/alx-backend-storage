8ea1cc83e0ffb3220a91524162097566a37bfab2
Selomon1 <soya.vandy1950@gmail.com>
2024-03-22 18:10:55 +0300

    MySQL Advanced

--- 0x00-MySQL_Advanced/8-index_my_names.sql
+++ 0x00-MySQL_Advanced/8-index_my_names.sql
@@ -1,10 +1,2 @@
 -- Create index idx_name_first on the first letter
-
--- Ensure the names table exists
-CREATE TABLE IF NOT EXISTS names (
-	id INT not null AUTO_INCREMENT,
-	name VARCHAR(255) NOT NULL,
-	first_letter CHAR(1) GENERATED ALWAYS AS (LEFT(name, 1)) STORED,
-	PRIMARY KEY (id),
-	INDEX idx_name_first (first_letter)
-);
+CREATE INDEX idx_name_first ON names(name(1));
