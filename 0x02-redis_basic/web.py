e4c3aeff07452f2e2e805124b8f8446bdf31f474
Selomon1 <soya.vandy1950@gmail.com>
2024-03-27 22:20:48 +0300

    Redis Basic

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -6,26 +6,47 @@ get page module
 import requests
 import redis
 import time
+from functools import wraps
 
 
-# Initialize Redis client
+# Initialize Redis Client
 redis_client = redis.Redis()
 
 
+def count_access(func):
+    """
+    Decorator to track URL access
+    """
+    @wraps(func)
+    def wrapper(url):
+        count_key = f"count:{url}"
+        redis_client.incr(count_key)
+        return func(url)
+    return wrapper
+
+
+def cache_expiry(func):
+    """
+    Decorator to cache
+    """
+    @wraps(func)
+    def wrapper(url):
+        cache_key = f"cache:{url}"
+        cached_result = redis_client.get(cache_key)
+        if cached_result:
+            return cached_result.decode('utf-8')
+        else:
+            result = dunc(url)
+            redis_client.setex(cache_key, 10, result)
+            return result
+        return wrapper
+
+
+@count_access
+@cache_expiry
 def get_page(url: str) -> str:
     """
     Fetch the HTML content of a given URL
     """
-    # Increment the count for the URL
-    count_key = f"count:{url}"
-    redis_client.incr(count_key)
-
-    # Get the HTML content
     response = requests.get(url)
-    html_content = response.text
-
-    # cache the HTML
-    cache_key = f"cache:{url}"
-    redis_client.setex(cache_key, 10, html_content)
-
-    return html_content
+    return response.text
