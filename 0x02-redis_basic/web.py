9eaa0f06f2ed7730815dc8f4da7597819333e671
Selomon1 <soya.vandy1950@gmail.com>
2024-03-27 23:01:55 +0300

    Redis Basic

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -9,11 +9,24 @@ from typing import Callable
 from functools import wraps
 
 
-def count_access(func):
+def count_access(call: Callable) -> Callable:
     """
     Decorator to track URL access
+    Param:
+        call: the call function which is Callable
+
+    Returns:
+        wrapper that tracks the count of access to url
     """
+    @wraps(call)
     def wrapper_get_page(url):
+        """
+        Retrieves the HTML content of the given URL
+        Params:
+            url: the URL
+        Returns:
+            the HTML retrieved from the URL
+        """
         redis_client = redis.Redis()
         redis_client.incr(f"count:{url}")
 
@@ -32,6 +45,10 @@ def count_access(func):
 def get_page(url: str) -> str:
     """
     Fetch the HTML content of a given URL
+    Params:
+        url: the given URL
+    Returns:
+        the HTML retrieved from the URL
     """
     response = requests.get(url)
     return response.text
